eb042c5497f2246bda4106fdd31fe1676c81da54
maherzaidoune <maherzaidoune@gmail.com>
2021-03-07 18:54:55 +0100

    add navigation service

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  */
 
 import {AppRegistry} from 'react-native';
+import 'react-native-gesture-handler';
 import App from './App';
 import {name as appName} from './app.json';
-
+if (!__DEV__) {
+	console.log = () => {};
+	console.time = () => {};
+	console.timeLog = () => {};
+	console.timeEnd = () => {};
+	console.warn = () => {};
+	console.count = () => {};
+	console.countReset = () => {};
+	console.error = () => {};
+	console.info = () => {};
+}
 AppRegistry.registerComponent(appName, () => App);
