8438629f29246b684af7e70dc13e76b61e07bd32
maherzaidoune <maherzaidoune@gmail.com>
2021-03-07 18:57:49 +0100

    implement react-native-screens config

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
 import App from './App';
 import {name as appName} from './app.json';
 if (!__DEV__) {
@@ -17,4 +18,5 @@ if (!__DEV__) {
 	console.error = () => {};
 	console.info = () => {};
 }
+enableScreens();
 AppRegistry.registerComponent(appName, () => App);
